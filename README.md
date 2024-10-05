c4a4bc214b6595780a9cc14c1414362f28c721e9
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-10-05 18:37:32 -0300

    :wrench: chore: config frontend to project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gamestore
 Store of hardwares, pc, machine!
+
+npx create-next-app@latest
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1> Game Store</h1>
+    </div>
+  );
+}
