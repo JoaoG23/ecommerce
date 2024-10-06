7adb6a6631cb7e4b7c057abcc42b9023af8f4d6c
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-10-06 09:57:19 -0300

    :wrench: chore: init backend and add config turbo app

---
+++ gam3r.store/apps/frontend/src/core/index.ts
@@ -0,0 +1,6 @@
+export * from './constants';
+export * from './parcelamento';
+export * from './produto'; 
+export * from './utils';
+// Todo que for exportado dentro 
+// de produto será exportado pra fora.
